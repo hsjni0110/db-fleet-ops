8fcdac52c547706ba9dc5796bf8599abe7c5f0da
한석진 <seokjin.han010@gmail.com>
2026-06-29 16:06:23 +0900

    chore: initialize spring boot gradle project

---
+++ src/main/java/com/dbfleetops/db_fleetops/DbFleetopsApplication.java
@@ -0,0 +1,13 @@
+package com.dbfleetops.db_fleetops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbFleetopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbFleetopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dbfleetops/db_fleetops/DbFleetopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbfleetops.db_fleetops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbFleetopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
